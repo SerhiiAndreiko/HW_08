de10b8b99bdb7aea59627e4df963ec65956fc3d9
SerhiiAndreiko <130772110+SerhiiAndreiko@users.noreply.github.com>
2023-06-29 00:22:34 +0200

    adde comments

--- HW_08.py
+++ HW_08.py
@@ -13,21 +13,22 @@ users = [
 
 
 def get_birthdays_per_week(users):
-    today = datetime.now().date()
-    start_of_week = today - timedelta(days=today.weekday())
-    end_of_week = start_of_week + timedelta(days=6)
+    today = datetime.now().date() # Поточна дата
+    start_of_week = today - timedelta(days=today.weekday()) # Початок поточного тижня (понеділок)
+    end_of_week = start_of_week + timedelta(days=6) # Кінець поточного тижня (неділя)
 
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     for i in range(7):
-        current_day = start_of_week + timedelta(days=i)
-        if current_day <= end_of_week:
+        current_day = start_of_week + timedelta(days=i) # Поточний день в межах тижня 
+        if current_day <= end_of_week: # Перевірка, чи день належить поточному тижню
             print(weekdays[i] + ':', end=' ')
             for user in users:
-                if user['birthday'].date() == current_day and current_day.weekday() < 5:
+                if user['birthday'].date() == current_day and current_day.weekday() < 5:  #Перевірка, чи це день народження та робочий день
                     print(user['name'], end=', ')
             print()
 
 
 get_birthdays_per_week(users)
+
 
